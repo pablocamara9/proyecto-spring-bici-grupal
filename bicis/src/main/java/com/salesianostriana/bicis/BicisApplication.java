859281e4f9b4d8aa0fb36a701248109af8a7b234
Pablo <camara.gapab23@triana.salesianos.edu>
2025-01-21 14:12:28 +0100

    Agregado proyecto de Spring

---
+++ bicis/src/main/java/com/salesianostriana/bicis/BicisApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.bicis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicisApplication.class, args);
+	}
+
+}
